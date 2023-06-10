2e6c9816cc5128fb32dbda62817ec91e22c36fe6
horodeski <geovana.horodeski06@gmail.com>
2023-06-09 22:06:48 -0300

    Create NavBar Component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+  <nav>
+    <img src="src/assets/images/logos/LogoHorizontal.svg" alt="">
+    <div class="links">
+        <RouterLink to="/">NOTÍCIAS</RouterLink>
+        <RouterLink to="/">PROJETOS</RouterLink>
+        <RouterLink to="/">MEMBROS</RouterLink>
+        <RouterLink to="/">PUBLICAÇÕES</RouterLink>
+    </div>
+    <div class="rede-sociais">
+      <box-icon style="margin: 0 8px" color="#fff" size="2em" name='github' type='logo' ></box-icon>
+      <box-icon style="margin: 0 8px" color="#fff" size="2em" type='logo' name='linkedin-square'></box-icon>
+      <box-icon style="margin: 0 8px" color="#fff" size="2em" type='logo' name='instagram'></box-icon>
+    </div>
+  </nav>
+</template>
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style/global.css'
+import 'boxicons'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
