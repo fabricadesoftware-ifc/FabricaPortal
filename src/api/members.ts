0542bfb77fced1c6b93d6246cfc77ac595624264
horodeski <geovana.horodeski06@gmail.com>
2023-07-08 23:27:59 -0300

    feat: Add GET endpoint for member by ID

--- src/api/members.ts
+++ src/api/members.ts
@@ -1,12 +1,20 @@
-import { members, type IMembers, occupations, type Ioccupations } from '@/_data/members'
+import { members, IMembers, occupations, Ioccupations } from '@/_data/members'
+import http from '../http-common'
+
 export default class MembersApi {
-    getMembers(): IMembers[] {
-        return members
-    }
-    getOccupations(): Ioccupations[] {
-        return occupations
-    }
-    getSixMembers(): IMembers[] {
-        return members.slice(0, 6)
-    }
+  getMembers(): IMembers[] {
+    return members
+  }
+
+  getMemberById(id: string): IMembers | undefined {
+    return members.find((member) => member.id === id)
+  }
+
+  getOccupations(): Ioccupations[] {
+    return occupations
+  }
+
+  getSixMembers(): IMembers[] {
+    return members.slice(0, 6)
+  }
 }
