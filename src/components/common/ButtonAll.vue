117044b80ed8fe0507d1f5b2ed894937731e666b
horodeski <geovana.horodeski06@gmail.com>
2023-07-15 02:07:04 -0300

    feat: Create hover effect in ButtonAll

--- src/components/common/ButtonAll.vue
+++ src/components/common/ButtonAll.vue
@@ -8,27 +8,31 @@ defineProps({
     type: String,
     required: true
   }
-});
+})
 </script>
 
 <template>
   <button>
-    <RouterLink :to="link" >{{ text }}</RouterLink>
+    <RouterLink :to="link">{{ text }}</RouterLink>
   </button>
 </template>
 
 <style scoped>
 button {
-  background-color: var(--background-color-btn-all);
-  border: none;
+  background-color: var(--primary-color);
+  border: 3px solid var(--primary-color);
   cursor: pointer;
   padding: 5px 7px;
   margin-top: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
+  transition: all ease-in-out 0.2s;
+  border-radius:5px;
 }
-
+button:hover{
+  background-color: #ff6600;
+}
 button a {
   font-size: 17px;
   color: var(--text-color-btn-all);
