e19870b055801b7497ceca01458ca2973524fc94
horodeski <geovana.horodeski06@gmail.com>
2023-07-16 01:00:07 -0300

    feat: Create NavBar effect on scroll

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute, useRouter } from 'vue-router';
+import {onMounted, ref, onUnmounted,} from 'vue'
+
+
 
 interface ILink {
   text?: string
@@ -20,12 +23,66 @@ const redesSociais: Array<ILink> = [
   {icon: "linkedin-square", to: "/"},
   {icon: "instagram", to: "/"},
 ]
+
+
+const logoResponId = ref<HTMLElement | null>(null)
+const logoId = ref<HTMLElement | null>(null)
+const navbar = ref<HTMLElement | null>(null)
+
+
+//! preciso que ao voltar para a HomeView,
+//! atualize a página para que o efeito
+//! do nav transparente volte a funcionar
+
+
+const returnToHome = () => {
+  const route = useRoute();
+  const router = useRouter();
+
+  if (route.path === '/') {
+    window.location.reload(); 
+  } else {
+    router.push('/'); 
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('load', scrollFunction)
+  window.addEventListener('scroll', scrollFunction)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', scrollFunction)
+})
+
+function scrollFunction(): void {
+  if (window.location.pathname === "/" && navbar.value && logoResponId.value && logoId.value) {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (scrollTop > 100) {
+      navbar.value.style.padding = "45px 145px";
+      logoResponId.value.style.opacity = "0";
+      logoResponId.value.style.display = "none";
+      logoId.value.style.display = "block";
+      logoId.value.style.opacity = "1";
+      navbar.value.style.backgroundColor = "#101215";
+    } else {
+      navbar.value.style.padding = "60px 145px";
+      logoId.value.style.opacity = "0";
+      logoId.value.style.display = "none";
+      logoResponId.value.style.display = "block";
+      logoResponId.value.style.opacity = "1";
+      navbar.value.style.backgroundColor = "transparent";
+    }
+  }
+}
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">
-      <img src="src/assets/images/logos/LogoHorizontal.svg" alt="" />
+  <nav id="navbar" ref="navbar">
+    <RouterLink @click="returnToHome()" to="/" exact  >
+      <img ref="logoResponId" id="logo-respon-id" src="src/assets/images/logos/LogoHorizontalCor.svg" alt="" />
+      <img  ref="logoId" id="logo-id" src="src/assets/images/logos/LogoHorizontal.svg" alt="" />
     </RouterLink>
     <div class="links">
       <a v-for="(link, i) in links" :class="link.active" :href="link.to" :key="i">{{ link.text }}</a>     
@@ -45,17 +102,22 @@ const redesSociais: Array<ILink> = [
 </template>
 
 <style scoped>
+#logo-respon-id {
+  display: none;
+}
 nav {
-  background-color: var(--background-color-nav);
+  overflow: hidden;
+  background-color: transparent;
   height: 73px;
-  padding: 0 145px;
+  padding: 40px 145px;
   position: fixed;
   width: -webkit-fill-available;
   width: -moz-available;
   justify-content: space-between;
   display: flex;
-  z-index: 2;
+  z-index: 99;
   align-items: center;
+  transition: 0.4s;
 }
 
 nav img {
