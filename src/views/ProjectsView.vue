bc2bd376b8ab422dfe33b8a3178e4c2daeed0a39
horodeski <geovana.horodeski06@gmail.com>
2023-07-22 01:20:18 -0300

    feat: Create ProjectsView

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import ProjectsApi from '@/api/projects'
+import ProjectsCard from "@/components/common/ProjectsCard.vue"
+
+const projectsApi = new ProjectsApi()
+const projects = ref([])
+
+onMounted(() => {
+  projects.value = projectsApi.getProjects()
+})
+</script>
+<template>
+  <main>
+    <section class="container">
+      <ProjectsCard
+        v-for="project of projects"
+        :key="project.id"
+        :title="project.title"
+        :description="project.description"
+        :logo="project.logo"
+        :image="project.image"
+        :type="project.type"
+        :linkProject="project"
+        :langsProject="project.langsProject"
+        :status="project.status"
+      />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding-top: 80px;
+}
+main .container {
+  background-color: var(--light-gray);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
