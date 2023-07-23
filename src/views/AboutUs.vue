7476870a2a08c1eeef54cd309a54c2c116771140
horodeski <geovana.horodeski06@gmail.com>
2023-07-23 00:30:19 -0300

    feat: Create AboutUs

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <main>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae illum iste, saepe dolorem quo
+    ducimus facere et magni, consequatur sit eveniet suscipit hic minima similique quia inventore,
+    odit itaque aliquam!
+  </main>
+</template>
+
+<style scoped>
+main {
+    padding-top: 100px;
+}
+</style>
