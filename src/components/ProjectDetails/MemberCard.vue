7b9b8d617d11cc5a0a8c3528727a2fcbb6dd2deb
horodeski <geovana.horodeski06@gmail.com>
2023-08-07 15:02:35 -0300

    feat: Create MemberCard in ProjectDetails

---
+++ src/components/ProjectDetails/MemberCard.vue
@@ -0,0 +1,87 @@
+<script setup>
+defineProps({
+    image: String,
+    name: String,
+    description: String,
+    occupation: {
+        type: Object
+    },
+    linkMember: {
+        type: Object
+    }
+})
+
+const shortenName = (name) => {
+    if (name.length > 25) {
+        const nameParts = name.split(' ')
+        const firstName = nameParts[0]
+        let shortenedName = firstName
+
+        if (nameParts.length > 2) {
+            for (let i = 1; i < nameParts.length - 1; i++) {
+                const middleName = nameParts[i]
+                if (middleName.length > 2) {
+                    shortenedName += ' ' + middleName[0] + '.'
+                }
+            }
+        }
+
+        shortenedName += ' ' + nameParts[nameParts.length - 1]
+        return shortenedName
+    }
+    return name
+}
+
+const UrlMember = (id) => {
+    return `/member/${id}`
+}
+</script>
+
+<template>
+    <router-link :to="UrlMember(linkMember.id)">
+        <div class="card">
+            <div class="header">
+                <img :src="image" class="image" />
+                <div>
+                    <h5>
+                        {{ occupation.description }}
+                    </h5>
+                    <h4>
+                        {{ shortenName(name) }}
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<style scoped>
+a {
+    width: 49%;
+    text-decoration: none;
+}
+
+.card {
+    margin: 10px 0;
+    padding: 10px;
+    background-color: var(--light-gray);
+    transition: transform var(--effect);
+    transform: scale(100%);
+}
+
+.card:hover {
+    transform: scale(97%);
+}
+
+.header {
+    display: flex;
+    gap: 10px;
+}
+
+
+.image {
+    height: 3rem;
+    width: 3rem;
+    object-fit: cover;
+}
+</style>
---
+++ src/components/ProjectDetails/MembersProject.vue
@@ -0,0 +1,37 @@
+<script setup>
+import MemberCard from '@/components/ProjectDetails/MemberCard.vue'
+import {defineProps} from 'vue'
+defineProps({
+  members: {
+    type: Array,
+    default: () => []
+  }
+})
+
+
+</script>
+
+<template>
+  <section v-if="members.length > 0">
+    <h3>Membros</h3>
+    <div class="members">
+      <MemberCard 
+        v-for="member in members" 
+        :key="member.id" 
+        :image="member.image" 
+        :name="member.name"
+        :description="member.description" 
+        :linkMember="member" 
+        :occupation="member.occupation" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section .members {
+  margin-bottom: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
