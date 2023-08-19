eeda4b488ad2f696aa3213090318199750b59435
horodeski <geovana.horodeski06@gmail.com>
2023-08-19 20:54:49 -0300

    feat: Update projectDetails

--- src/components/ProjectDetails/DescriptionProject.vue
+++ src/components/ProjectDetails/DescriptionProject.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { ref } from 'vue';
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 defineProps({
     description: String,
     logo: String,
-    title: String
+    title: String,
+    type: String
 });
 
 const modalHidden = ref(true);
@@ -25,13 +25,13 @@ const toggleModal = () => {
     <section class="description">
         <header>
             <h3>Sobre esse projeto</h3>
-            <button @click="toggleModal">
+            <button class="btn-arrow" @click="toggleModal">
                 <box-icon name='right-arrow-alt'></box-icon>
             </button>
         </header>
         <p class="short-bio">{{ description }}</p>
     </section>
-    <div class="modal-overlay" v-body-scroll-lock="!modalHidden" :class="{ 'hide': modalHidden }" ></div>
+    <div class="modal-overlay" v-body-scroll-lock="!modalHidden" :class="{ 'hide': modalHidden }"></div>
     <section id="modal-content" :class="[{ hide: modalHidden }]">
         <header>
             <div>
@@ -40,6 +40,93 @@ const toggleModal = () => {
             </div>
             <button @click="toggleModal"><box-icon name='x'></box-icon></button>
         </header>
+        <main>
+            <section class="details">
+                <h4>
+                    Detalhes
+                </h4>
+                <ul>
+                    <li>
+                        <h5>Tipo:</h5>
+                        <span>Extensão</span>
+                    </li>
+                    <li>
+                        <h5>Status:</h5>
+                        <span>Em andamento</span>
+                    </li>
+                    <li>
+                        <h5>Versão:</h5>
+                        <span>1.0.1</span>
+                    </li>
+                    <li>
+                        <h5>Atualizado em:</h5>
+                        <span>13 de ago. de 2023</span>
+                    </li>
+                    <li>
+                        <h5>Cordenador(es):</h5>
+                        <div>
+                            <button>Eduardo da Silva</button>
+                            <button>Fábio longo de Moura</button>
+                        </div>
+                    </li>
+                    <li>
+                        <h5>Publicado em:</h5>
+                        <span>27 de jun. de 2023</span>
+                    </li>
+                    <li>
+                        <h5>Tecnologias:</h5>
+                        <ul id="content-lang">
+                            <li>
+                                <button>
+                                    vue
+                                </button>
+                            </li>
+                            <li>
+                                <button>
+                                    react
+                                </button>
+                            </li>
+                            <li>
+                                <button>
+                                    django
+                                </button>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </section>
+            <section class="access">
+                <h4>Acesso</h4>
+                <ul>
+                    <li>
+                        <button>
+                            <box-icon type='logo' name='github'></box-icon>
+                            Github
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <box-icon name='windows'></box-icon>
+                            Deploy
+                        </button>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h4>Novidades</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquid odit porro assumenda unde dolore
+                    eius minima, voluptas quis veniam, aperiam sint dolorum sed corporis vel impedit debitis odio dolores.
+                </p>
+            </section>
+            <section class="tags">
+                <h4>Tags</h4>
+                <ul>
+                    <li v-for="li in 13" :key="li">
+                        <button>hackathon</button>
+                    </li>
+                </ul>
+            </section>
+        </main>
         <div class="body">
         </div>
     </section>
@@ -58,12 +145,78 @@ header {
     align-items: flex-start;
 
 }
+
 body.modal-open {
     overflow: hidden;
 }
 
-#modal-content header {
+#modal-content main {
+    padding: 10px 20px;
+    overflow-y: scroll;
+    height: 65vh;
+}
+
+ul {
+    list-style-type: none;
+}
+
+li ul {
+    display: flex;
+}
+.access ul{
+    display: flex;
+    gap: 10px
+}
+#content-lang {
+    justify-content: flex-start;
+    padding: 0
+}
+
+#content-lang li {
+    width: fit-content;
+}
+
+.details ul {
+    padding: 10px;
+    display: flex;
+    gap: 12px;
+    flex-wrap: wrap;
     justify-content: space-between;
+}
+
+.details li {
+    display: flex;
+    flex-direction: column;
+    width: 48%;
+}
+
+button {
+    background-color: var(--light-gray);
+    padding: 4px;
+    border-radius: 5px;
+    border: none;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+}
+
+.tags ul {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+#modal-content section p {
+    padding: 10px;
+}
+
+#modal-content section {
+    margin: 15px 0;
+}
+
+#modal-content header {
+    box-shadow: var(--box-shadow);
+    justify-content: space-between;
+    padding: 10px
 }
 
 #modal-content header div {
@@ -72,7 +225,7 @@ body.modal-open {
     align-items: center;
 }
 
-button {
+.btn-arrow {
     border-radius: 50%;
     border: none;
     padding: 5px;
@@ -119,19 +272,13 @@ main .midias {
     top: 50%;
     transform: translate(-50%, -50%);
     width: 50%;
-    background-color: var(--light-gray);
-    padding: 10px;
+    background-color: var(--white);
     z-index: 100;
     transition: opacity 0.2s, pointer-events 0.2s;
-}
-
-
-#modal-content {
-    transition: 0.2s;
     opacity: 1;
     pointer-events: all;
+    height: 80vh
 }
-
 
 #modal-content.hide,
 .hide {
@@ -141,5 +288,4 @@ main .midias {
 
 .logo {
     width: 5em;
-}
-</style>
+}</style>
--- src/components/ProjectDetails/ProjectHeader.vue
+++ src/components/ProjectDetails/ProjectHeader.vue
@@ -12,70 +12,27 @@ defineProps({
 <template>
   <header>
     <img class="logo" :src="logo" alt="" />
-    <div class="title-status">
       <h2>{{ title }}</h2>
-      <div class="links">
-        <a href=""><box-icon color="var(--secondary-color)" name='windows'></box-icon> Ver o projeto </a>
-        <a href=""><box-icon color="var(--secondary-color)" type='logo' name='github'></box-icon> Repositório no github</a>
-        <a href=""><box-icon color="var(--secondary-color)" name='cloud-download'></box-icon> Download</a>
-      </div>
-    </div>
+
   </header>
 </template>
 <style scoped>
-/*padronizar os h2*/
 h2 {
   color: var(--text-color);
   margin: 0;
-}
-
-header h3 {
-  color: var(--members);
-  text-transform: uppercase;
-  font-weight: 600;
-}
-
-.title-status {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+  text-align: center;
 }
 
 header {
   width: 100%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
 }
 
 .logo {
   max-width: 100px;
   height: fit-content;
   margin-right: 15px;
-}
-
-.links {
-  display: flex;
-  gap: 15px;
-}
-
-a {
-  color: var(--text-color);
-  text-decoration: none;
-  display: flex;
-  align-items: center;
-  gap: 5px;
-}
-
-a:hover {
-  color: var(--secondary-color);
-  text-decoration: underline;
-
-}
-
-.details div {
-  display: flex;
-  align-items: center;
-}
-
-section {
-  margin-bottom: 14px;
 }</style>
