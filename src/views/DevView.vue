f439c845941402e2ae973d8a9e1886180548171c
horodeski <geovana.horodeski06@gmail.com>
2023-12-16 12:03:09 -0300

    feat: Adjust responsive maintenance screen

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -3,9 +3,9 @@
 <template>
   <main>
     <section>
-      <img src="../assets/images/construction.jpg" alt="">
-      <h1>Volte mais tarde!</h1>
-      <p>Está página está em desenvolvimento</p>
+      <img src="../assets/images/construction.jpg" alt="" />
+      <h2>Ops! Estamos trabalhando nisso</h2>
+      <p>Nossa página está passando por melhorias.</p>
     </section>
   </main>
 </template>
@@ -20,7 +20,21 @@ main section {
   flex-direction: column;
   border-radius: 0;
 }
+h2,
+p {
+  text-align: center;
+}
 img {
-  width: 20em;
+  width: 30%;
+}
+@media only screen and (max-width: 768px) {
+  img {
+    width: 60%;
+  }
+}
+@media only screen and (max-width: 600px) {
+  img {
+    width: 70%;
+  }
 }
 </style>
