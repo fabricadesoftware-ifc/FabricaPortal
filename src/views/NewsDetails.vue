e030114f1f047d2ce665d3210daed6e9ec5a851b
Caussz <cczcaua@gmail.com>
2024-04-11 18:20:54 -0300

    chore[NewsDetails]: Updating the NewsDetails with the rest of the pertinent information

--- src/views/NewsDetails.vue
+++ src/views/NewsDetails.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted} from 'vue'
 import NewsHeader from '@/components/NewsDetails/NewsHeader.vue';
 import CarouselComp from '@/components/ProjectDetails/CarouselComp.vue'
+import DescriptionNews from '@/components/NewsDetails/DescripitionNews.vue';
 import NewsApi from '@/api/news'
 
 const newsApi = new NewsApi()
@@ -14,6 +15,7 @@ onMounted(async () => {
   const newsIdFromUrl = path.substring(path.lastIndexOf('/') + 1)
   newsId.value = newsIdFromUrl
   news.value = newsApi.getNewsById(newsId.value)
+  console.log(news.value);
 })
 
 </script>
@@ -22,7 +24,7 @@ onMounted(async () => {
   <main v-if="news">
     <NewsHeader :title="news.title" />
     <CarouselComp :images="news.images" />
-
+<DescriptionNews :description="news.description" :logo="news.logo" :tags="news.tags" :published="news.published" :status="news.status" :updated="news.updated" :title="news.title" />
   </main>
 </template>
 
