2137dcdc380c9a9f3335f8594979c6fceb2bc13c
Hiansdt <vnhiansdt@gmail.com>
2024-06-06 17:08:49 -0300

    FEAT: Add vue-multiselect library and declare box-icons usage

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => tag === 'box-icon'
+      }
+    }
+  }), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
